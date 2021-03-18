4003d15391833b82ccfb0495995be458587f420e
AYUSH MISHRA <36783003+ayushmishra6198@users.noreply.github.com>
2021-03-18 21:47:06 +0530

    Update UserController.java

--- src/main/java/com/employeemgmt/Controller/UserController.java
+++ src/main/java/com/employeemgmt/Controller/UserController.java
@@ -5,16 +5,12 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.apache.jasper.tagplugins.jstl.core.ForEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.employeemgmt.Domain.Employee;
 import com.employeemgmt.Domain.User;
